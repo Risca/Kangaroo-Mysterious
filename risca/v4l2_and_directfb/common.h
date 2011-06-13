eb6eb54c920bb19edfc3471d12da9de976785ad2
Patrik Dahlström <patda293@student.liu.se>
2011-06-13 20:36:36 +0200

    Initial commit
    Signed-off-by: Patrik Dahlström <patda293@student.liu.se>

---
+++ risca/v4l2_and_directfb/common.h
@@ -0,0 +1,25 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <errno.h>
+
+void errno_exit(const char *s)
+{
+        fprintf (stderr, "%s error %d, %s\n",
+                 s, errno, strerror (errno));
+
+        exit (EXIT_FAILURE);
+}
+
+int xioctl(int fd,int request,void *arg)
+{
+        int r;
+
+        do r = ioctl (fd, request, arg);
+        while (-1 == r && EINTR == errno);
+
+        return r;
+}
+
+#endif // COMMON_H
---
+++ risca/v4l2_and_directfb/directfb_inc.c
@@ -0,0 +1,69 @@
+#include "directfb_inc.h"
+
+static IDirectFB *dfb = NULL;
+static IDirectFBFont *font = NULL;
+static IDirectFBSurface *primary = NULL;
+//static DFBRectangle rect;
+static int screen_width  = 0;
+static int screen_height = 0;
+static char *title = "Kangaroo-Mysterius";
+
+int directfb_init(int argc, char *argv[])
+{
+    DFBSurfaceDescription dsc;
+    DFBFontDescription font_dsc;
+
+    DFBCHECK (DirectFBInit (&argc, &argv));
+    DFBCHECK (DirectFBCreate (&dfb));
+    DFBCHECK (dfb->SetCooperativeLevel (dfb, DFSCL_NORMAL));
+    dsc.flags = DSDESC_CAPS | DSDESC_PIXELFORMAT;
+    dsc.caps  = DSCAPS_PRIMARY | DSCAPS_FLIPPING;
+    dsc.pixelformat = DSPF_YUY2;
+    DFBCHECK (dfb->CreateSurface( dfb, &dsc, &primary ));
+    DFBCHECK (primary->GetSize (primary, &screen_width, &screen_height));
+
+    font_dsc.flags = DFDESC_HEIGHT;
+    font_dsc.height = 48;
+    DFBCHECK (dfb->CreateFont (dfb, "Vera.ttf", &font_dsc, &font));
+    DFBCHECK (primary->SetFont(primary, font));
+
+    /* Blank screen */
+    DFBCHECK (primary->SetColor (primary, 0x0, 0x0, 0x0, 0xFF));
+    DFBCHECK (primary->FillRectangle (primary, 0, 0, screen_width, screen_height));
+    /* Print text */
+    DFBCHECK (primary->SetColor (primary, 0x80, 0x0, 0x20, 0xFF));
+    DFBCHECK (primary->DrawString (primary, title, -1, 10, screen_height / 2, DSTF_LEFT));
+    /* Flip the front and back buffer, but wait for the vertical retrace to avoid tearing. */
+    DFBCHECK (primary->Flip (primary, NULL, DSFLIP_WAITFORSYNC));
+
+    font->Release (font);
+    return 23;
+}
+
+void CopyYUVData(const void *src, IDirectFBSurface *des)
+{
+    int y;
+    void             *data;
+    int               pitch;
+    DFBResult         ret;
+    ret = des->Lock (des, DSLF_WRITE, &data, &pitch);
+    if (ret)
+    {
+        fprintf(stderr,"IDirectFBSurface:ock error code =%d", ret);
+        return;
+    }
+    for(y=0; y<YUVSRC_H; y++)
+    {
+        memcpy(data, src+YUVFRAME_Pitch*y, YUVFRAME_Pitch);
+        data += pitch;
+    }
+    des->Unlock (des);
+}
+
+int directfb_render(const void *p)
+{
+    CopyYUVData(p, primary);
+    DFBCHECK (primary->Flip (primary, NULL, DSFLIP_WAITFORSYNC));
+
+    return 0;
+}
