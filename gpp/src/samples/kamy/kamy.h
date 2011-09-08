64ac93cd787f1eaeb3a5640e0683f1a25d5c0e3b
Patrik Dahlström <patrikd@telia.com>
2011-09-08 21:22:02 +0200

    Added declaration of KM_displayFrame
    Signed-off-by: Patrik Dahlström <patrikd@telia.com>

--- gpp/src/samples/kamy/kamy.h
+++ gpp/src/samples/kamy/kamy.h
@@ -215,6 +215,17 @@ NORMAL_API
 DSP_STATUS
 KM_OS_init (Void) ;
 
+/** ============================================================================
+ *  @func	KM_displayFrame
+ *
+ *  @desc	Shows a YUV:422 image on the screen
+ *
+ *  @modif	None.
+ *  ============================================================================
+ */
+void
+KM_displayFrame(Uint32 width, Uint32 height, Uchar8 *image_ptr ) ;
+
 
 /** ============================================================================
  *  @func   KM_OS_exit
